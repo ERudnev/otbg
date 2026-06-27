1ebb32ca64ee618207036504b3452236fe2938b4
Rever <mortazar441@gmail.com>
2026-06-27 08:55:05 +0300

    infra: INCOMPLETE switched to crash-mode

--- src/NewGame/Internals/macros.h
+++ src/NewGame/Internals/macros.h
@@ -1,3 +1,7 @@
 #pragma once
 
-#define _INCOMPLETE_ do {} while (false)
+#include <format>
+#include <stdexcept>
+
+//#define _INCOMPLETE_ do {} while (false)
+#define _INCOMPLETE_ throw std::logic_error(std::format("{}:{}: INCOMPLETE", __FILE__, __LINE__));
--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -60,59 +60,67 @@ int main(int, char**)
 
     for (const auto& [name, scenario] : bucket)
     {
-        game::InjectedRandomDevice::init(0);
-        EventSystem events;
-        game::World world(events);
-
-        io::CommandParser parser;
-        parser.add<io::CreateMap>([&world](auto command) { world.createMap(command.width, command.height); });
-        parser.add<io::SpawnSwordsman>(
-                [&world](auto command)
-                {
-                    world.spawnUnit(
-                            command.unitId,
-                            {command.x, command.y},
-                            command.hp,
-                            command.strength,
-                            command.chance,
-                            command.rending);
-                });
-        parser.add<io::SpawnHunter>(
-                [](auto command)
-                {
-                    //TODO
-                });
-        parser.add<io::March>([&world](auto command) { world.march(command.unitId, {command.targetX, command.targetY}); });
-
-        tests::Strings grabbed;
+        try
         {
-            CoutCapture capture;
-            auto input = serialise(scenario.commands);
+            game::InjectedRandomDevice::init(0);
+            EventSystem events;
+            game::World world(events);
 
-            parser.parse(input);
+            io::CommandParser parser;
 
-            while (!world.isGameOver())
+            parser.add<io::CreateMap>([&world](auto command) { world.createMap(command.width, command.height); });
+            parser.add<io::SpawnSwordsman>(
+                    [&world](auto command)
+                    {
+                        world.spawnUnit(
+                                command.unitId,
+                                {command.x, command.y},
+                                command.hp,
+                                command.strength,
+                                command.chance,
+                                command.rending);
+                    });
+            parser.add<io::SpawnHunter>(
+                    [](auto command)
+                    {
+                        //TODO
+                    });
+            parser.add<io::March>([&world](auto command) { world.march(command.unitId, {command.targetX, command.targetY}); });
+
+            tests::Strings grabbed;
             {
-                world.step();
+                CoutCapture capture;
+                auto input = serialise(scenario.commands);
+
+                parser.parse(input);
+
+                while (!world.isGameOver())
+                {
+                    world.step();
+                }
+
+                grabbed = capture.lines();
             }
 
-            grabbed = capture.lines();
-        }
+            const auto reports = tests::Usage::checkEqual(grabbed, scenario.expectations);
 
-        const auto reports = tests::Usage::checkEqual(grabbed, scenario.expectations);
-
-        if (reports.empty())
-        {
-            std::cout << std::format(R"(OK: Scenario "{}")", name) << std::endl;
-        }
-        else
-        {
-            ++failedCount;
-            std::cout << std::format(R"(FAIL: Scenario "{}")", name) << std::endl;
-            for (const auto& report : reports)
+            if (reports.empty())
             {
-                std::cout << std::format("  {}", report) << std::endl;
+                std::cout << std::format(R"(OK: Scenario "{}")", name) << std::endl;
             }
+            else
+            {
+                ++failedCount;
+                std::cout << std::format(R"(FAIL: Scenario "{}")", name) << std::endl;
+                for (const auto& report : reports)
+                {
+                    std::cout << std::format("  {}", report) << std::endl;
+                }
+            }
+        }
+        catch (std::exception& e)
+        {
+            std::cout << std::format(R"(CRITICAL: Scenario "{}": {})", name, e.what()) << std::endl;
         }
     }
 
